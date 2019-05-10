0c49b7f39d4d867967f4a50b3385f8ecb5c07e6d
Richard Wall <richard.wall@jetstack.io>
2019-05-10 17:03:41 +0100

    Begin implementing the validation hook

--- cassandra-operator/pkg/apis/cassandra/validation/webhooks/cassandra.go
+++ cassandra-operator/pkg/apis/cassandra/validation/webhooks/cassandra.go
@@ -1,4 +1,27 @@
 package webhooks
 
+import (
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/apis/cassandra/v1alpha1"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	restclient "k8s.io/client-go/rest"
+)
+
 type Cassandra struct {
+}
+
+func (c *Cassandra) Initialize(kubeClientConfig *restclient.Config, stopCh <-chan struct{}) error {
+	return nil
+}
+
+func (c *Cassandra) ValidatingResource() (plural schema.GroupVersionResource, singular string) {
+	gv := v1alpha1.SchemeGroupVersion
+	gv.Group = "admission." + gv.Group
+	// override version to be the version of the admissionresponse resource
+	gv.Version = "v1beta1"
+	return gv.WithResource("cassandras"), "cassandra"
+}
+
+func (c *Cassandra) Validate(admissionSpec *admissionv1beta1.AdmissionRequest) *admissionv1beta1.AdmissionResponse {
+	return nil
 }
--- cassandra-webhook/cmd/webhook/main.go
+++ cassandra-webhook/cmd/webhook/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/openshift/generic-admission-server/pkg/cmd"
+	"github.com/sky-uk/cassandra-operator/cassandra-webhook/pkg/apis/cassandra/validation/webhooks"
 )
 
+var hook cmd.ValidatingAdmissionHook = &webhooks.Cassandra{}
+
 func main() {
-	cmd.RunAdmissionServer()
+	cmd.RunAdmissionServer(hook)
 }
