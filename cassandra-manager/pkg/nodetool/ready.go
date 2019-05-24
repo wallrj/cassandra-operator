daf35484a2ad5c12a9ec8a64a218baab2eec288d
Richard Wall <richard.wall@jetstack.io>
2019-05-24 09:55:26 +0000

    Enough to test for connection refused

--- cassandra-manager/pkg/nodetool/ready.go
+++ cassandra-manager/pkg/nodetool/ready.go
@@ -1,6 +1,7 @@
 package nodetool
 
 import (
+	"log"
 	"time"
 
 	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/cluster"
@@ -14,9 +15,23 @@ func (l *localGatherer) UrlFor(*cluster.Cluster) string {
 	return "http://127.0.0.1:7777"
 }
 
-func IsLocalNodeReady() bool {
+type Nodetool struct {
+	cluster *cluster.Cluster
+}
+
+func New(cluster *cluster.Cluster) *Nodetool {
+	return &Nodetool{cluster: cluster}
+}
+
+func (n *Nodetool) IsLocalNodeReady() (bool, error) {
 	j := &localGatherer{}
-	gatherer := metrics.NewGatherer(j, &metrics.Config{RequestTimeout: 20 * time.Second})
-	status := gatherer.GatherMetricsFor(&cluster.Cluster{})
-	return true
+	gatherer := metrics.NewGatherer(j, &metrics.Config{
+		RequestTimeout: 20 * time.Second,
+	})
+	status, err := gatherer.GatherMetricsFor(n.cluster)
+	if err != nil {
+		return false, err
+	}
+	log.Println(status)
+	return true, nil
 }
---
+++ cassandra-manager/pkg/nodetool/ready_test.go
@@ -0,0 +1,38 @@
+package nodetool_test
+
+import (
+	"testing"
+
+	"github.com/sky-uk/cassandra-operator/cassandra-manager/pkg/nodetool"
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/apis/cassandra/v1alpha1"
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/cluster"
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/util/ptr"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+func Test(t *testing.T) {
+	c, err := cluster.New(
+		&v1alpha1.Cassandra{
+			Spec: v1alpha1.CassandraSpec{
+				UseEmptyDir: ptr.Bool(true),
+				Racks: []v1alpha1.Rack{
+					{
+						Name:     "rack1",
+						Zone:     "a",
+						Replicas: 1,
+					},
+				},
+				Pod: v1alpha1.Pod{
+					Memory: resource.MustParse("1Gi"),
+				},
+			},
+		},
+	)
+	require.NoError(t, err)
+	nt := nodetool.New(c)
+	ready, err := nt.IsLocalNodeReady()
+	require.NoError(t, err)
+	assert.True(t, ready)
+}
