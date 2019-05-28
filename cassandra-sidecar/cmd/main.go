fe360b92ecfe744fad928a3377e6b0cf816f03b6
Richard Wall <richard.wall@jetstack.io>
2019-05-28 16:11:17 +0000

    Use logrus and cobra, for consistency with other commands

--- cassandra-sidecar/cmd/main.go
+++ cassandra-sidecar/cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 
 	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/apis/cassandra/v1alpha1"
 	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/cluster"
@@ -12,22 +14,63 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+var (
+	logLevel         string
+	nodeAddress      string
+	clusterName      string
+	clusterNamespace string
+)
+
+var rootCmd = &cobra.Command{
+	Use:               "cassandra-sidecar",
+	Short:             "Sidecar for interacting with Cassandra nodes",
+	PersistentPreRunE: handleArgs,
+	RunE:              startOperator,
+}
+
 func main() {
-	nodeAddress := os.Getenv("NODE_LISTEN_ADDRESS")
+	if err := rootCmd.Execute(); err != nil {
+		log.WithError(err).Fatal("Execute failed")
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", log.InfoLevel.String(), "should be one of: debug, info, warn, error, fatal, panic")
+}
+
+func handleArgs(_ *cobra.Command, _ []string) error {
+	level, err := log.ParseLevel(logLevel)
+	if err != nil {
+		return fmt.Errorf("invalid log-level")
+	}
+	log.SetLevel(level)
+
+	nodeAddress = os.Getenv("NODE_LISTEN_ADDRESS")
 	if nodeAddress == "" {
-		log.Fatal("NODE_LISTEN_ADDRESS must be set")
+		return fmt.Errorf("NODE_LISTEN_ADDRESS must be set")
 	}
 
-	clusterName := os.Getenv("CLUSTER_NAME")
+	clusterName = os.Getenv("CLUSTER_NAME")
 	if clusterName == "" {
-		log.Fatal("CLUSTER_NAME must be set")
+		return fmt.Errorf("CLUSTER_NAME must be set")
 	}
 
-	clusterNamespace := os.Getenv("CLUSTER_NAMESPACE")
+	clusterNamespace = os.Getenv("CLUSTER_NAMESPACE")
 	if clusterNamespace == "" {
-		log.Fatal("CLUSTER_NAMESPACE must be set")
+		return fmt.Errorf("CLUSTER_NAMESPACE must be set")
 	}
 
+	return nil
+}
+
+func startOperator(_ *cobra.Command, _ []string) error {
+	logger := log.WithFields(
+		log.Fields{
+			"clusterName":      clusterName,
+			"clusterNamespace": clusterNamespace,
+			"nodeAddress":      nodeAddress,
+		},
+	)
 	nt := metrics.NewNodetool(
 		cluster.NewWithoutValidation(
 			&v1alpha1.Cassandra{
@@ -41,32 +84,34 @@ func main() {
 	)
 
 	http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("/ready called")
 		ready, err := nt.IsNodeReady(nodeAddress)
 		if err != nil {
-			log.Println(err)
+			w.WriteHeader(500)
+			logger.WithError(err).Error("IsNodeReady failed")
+			return
 		}
 		if !ready {
 			w.WriteHeader(500)
-			log.Println("IsNodeReady failed")
+			return
 		}
-		fmt.Fprintf(w, "cassandra node status: ready: %#v", ready)
+		_, err = fmt.Fprintf(w, "ok")
+		if err != nil {
+			log.WithError(err).Error("Fprintf failed")
+		}
 	})
 
 	http.HandleFunc("/live", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("/live called")
-
-		status := "succeeded"
-
 		_, err := nt.IsNodeReady(nodeAddress)
 		if err != nil {
-			log.Println(err)
 			w.WriteHeader(500)
-			log.Println("IsNodeReady failed")
-			status = "failed"
+			logger.WithError(err).Error("IsNodeReady failed")
+			return
 		}
-		fmt.Fprintf(w, "cassandra node status: %s", status)
+		_, err = fmt.Fprintf(w, "ok")
+		if err != nil {
+			log.WithError(err).Error("Fprintf failed")
+		}
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	return http.ListenAndServe(":8080", nil)
 }
