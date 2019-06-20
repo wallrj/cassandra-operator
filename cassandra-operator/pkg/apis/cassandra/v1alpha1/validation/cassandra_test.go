a8bc59e8c397a6ca851b78275cd87845614c4fd6
Richard Wall <richard.wall@jetstack.io>
2019-06-20 18:08:30 +0100

    A more orthodox testing approach

--- cassandra-operator/pkg/apis/cassandra/v1alpha1/validation/cassandra_test.go
+++ cassandra-operator/pkg/apis/cassandra/v1alpha1/validation/cassandra_test.go
@@ -2,14 +2,10 @@ package validation_test
 
 import (
 	"fmt"
-	"reflect"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/kr/pretty"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -24,37 +20,15 @@ func TestCassandra(t *testing.T) {
 	RunSpecsWithDefaultAndCustomReporters(t, "Cluster Suite", test.CreateParallelReporters("cluster"))
 }
 
-func set(d interface{}, path string, value interface{}) {
-	segments := strings.Split(path, ".")
-	v := reflect.ValueOf(d)
-	for _, s := range segments {
-		for v.Kind() == reflect.Ptr {
-			v = v.Elem()
-		}
-		v = index(v, s)
-	}
-	v.Set(reflect.ValueOf(value))
-}
-
-func index(v reflect.Value, idx string) reflect.Value {
-	if i, err := strconv.Atoi(idx); err == nil {
-		return v.Index(i)
-	}
-	return v.FieldByName(idx)
-}
-
-type mutator func(*v1alpha1.Cassandra)
-
-func mutate(path string, value interface{}) mutator {
-	return func(c *v1alpha1.Cassandra) {
-		set(c, path, value)
-	}
-}
-
 var _ = Describe("validation functions", func() {
 	Context("ValidateCassandra", func() {
-		var cass *v1alpha1.Cassandra
+		var (
+			cass *v1alpha1.Cassandra
+			err  error
+		)
+
 		BeforeEach(func() {
+			err = nil
 			cass = &v1alpha1.Cassandra{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "cluster1",
@@ -72,47 +46,56 @@ var _ = Describe("validation functions", func() {
 				},
 			}
 		})
-		DescribeTable(
-			"success cases",
-			func(mutations ...mutator) {
-				for _, mutate := range mutations {
-					mutate(cass)
-				}
-				By(pretty.Sprintf("validating %# v", *cass))
-				err := validation.ValidateCassandra(cass).ToAggregate()
-				Expect(err).ToNot(HaveOccurred())
-			},
-			Entry("complete cluster"),
-		)
 
-		DescribeTable(
-			"failure cases",
-			func(mutations ...mutator) {
-				for _, mutate := range mutations {
-					mutate(cass)
-				}
+		Context("success cases", func() {
+			AfterEach(func() {
 				By(pretty.Sprintf("validating %# v", *cass))
-				err := validation.ValidateCassandra(cass).ToAggregate()
+				err = validation.ValidateCassandra(cass).ToAggregate()
+				Expect(err).ToNot(HaveOccurred())
+			})
+			It("succeeds with a fully populated Cassandra object", func() {
+
+			})
+		})
+
+		Context("failure cases", func() {
+			AfterEach(func() {
+				By(pretty.Sprintf("validating %# v", *cass))
+				err = validation.ValidateCassandra(cass).ToAggregate()
 				fmt.Fprintf(GinkgoWriter, "INFO: Error message was: %s", err)
 				Expect(err).To(HaveOccurred())
-			},
-			// ObjectMeta
-			Entry("missing name", mutate("ObjectMeta.Name", "")),
-			Entry("missing namespace", mutate("ObjectMeta.Namespace", "")),
+			})
 
-			// Spec.Racks
-			Entry("missing racks", mutate("Spec.Racks", []v1alpha1.Rack{})),
-			Entry("rack with 0 replicas", mutate("Spec.Racks.0.Replicas", int32(0))),
-			Entry(
-				"rack with empty storageClass",
-				mutate("Spec.UseEmptyDir", ptr.Bool(false)),
-				mutate("Spec.Racks.0.StorageClass", ""),
-			),
-			Entry(
-				"rack with empty zone",
-				mutate("Spec.UseEmptyDir", ptr.Bool(false)),
-				mutate("Spec.Racks.0.Zone", ""),
-			),
-		)
+			Context("ObjectMeta", func() {
+				It("fails if name is missing", func() {
+					cass.Name = ""
+				})
+				It("fails if namespace is missing", func() {
+					cass.Namespace = ""
+				})
+			})
+
+			Context("Spec", func() {
+				Context("Racks", func() {
+					It("fails if racks is empty", func() {
+						cass.Spec.Racks = nil
+					})
+					It("fails if Rack.Replicas is < 1", func() {
+						cass.Spec.Racks[0].Replicas = 0
+					})
+					Context("UseEmptyDir=false", func() {
+						BeforeEach(func() {
+							cass.Spec.UseEmptyDir = ptr.Bool(false)
+						})
+						It("fails if Rack.StorageClass is empty", func() {
+							cass.Spec.Racks[0].StorageClass = ""
+						})
+						It("fails if Rack.Zone is empty", func() {
+							cass.Spec.Racks[0].Zone = ""
+						})
+					})
+				})
+			})
+		})
 	})
 })
