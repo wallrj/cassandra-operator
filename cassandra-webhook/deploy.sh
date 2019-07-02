63bc36744837ca67d3d5efcfa07465874d3ebf3e
Richard Wall <richard.wall@jetstack.io>
2019-07-02 14:44:37 +0000

    Generate a self-signed certificate for the webhook

--- cassandra-webhook/deploy.sh
+++ cassandra-webhook/deploy.sh
@@ -38,25 +38,63 @@ function waitForDeployment {
 tmpDir=$(mktemp -d)
 trap '{ CODE=$?; rm -rf ${tmpDir} ; exit ${CODE}; }' EXIT
 
+
+function create_certificates() {
+    local fqdn="${1}"
+    mkdir "${tmpDir}/pki"
+    # See https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster/
+    cat <<EOF > "${tmpDir}/pki/cfssl.json"
+{
+  "hosts": [
+      "$fqdn"
+  ],
+  "CN": "${fqdn}",
+  "key": {
+    "algo": "ecdsa",
+    "size": 256
+  }
+}
+EOF
+
+    cfssl selfsign "${fqdn}" "${tmpDir}/pki/cfssl.json" \
+        | cfssljson -bare "${tmpDir}/pki/server"
+
+    ls -l "${tmpDir}/pki"
+
+    kubectl create secret tls \
+            --dry-run \
+            --output yaml \
+            --namespace test-cassandra-operator \
+            --cert "${tmpDir}/pki/server.pem" \
+            --key="${tmpDir}/pki/server-key.pem" \
+            cassandra-operator-webhook-tls > "${tmpDir}/secret.yaml"
+}
+
 function deploy() {
     local image=$1
     local context=$2
     local namespace=$3
     local deployment=cassandra-webhook
 
-    kind load docker-image "${image}"
-    kubectl delete namespace "${namespace}" || true
-    until kubectl create namespace "${namespace}"; do sleep 1; done
+    create_certificates "webhook-service.${namespace}.svc"
 
-    kubectl -n "${namespace}" create secret tls --key tls.key --cert tls.crt webhook-tls
-    k8Resources="webhook-deployment.yaml manifests.yaml"
+    kubectl label namespace $namespace webhooks.cassandra.core.sky.uk=enabled
+
+    TARGET_NAMESPACE=$namespace \
+        CA_BUNDLE="$(cat ${tmpDir}/pki/server.pem)" \
+        go run ./hack/munge-webhook.go ./kubernetes-resources/manifests.yaml > $tmpDir/manifests.yaml
+
+    #kubectl -n "${namespace}" create secret tls --key tls.key --cert tls.crt webhook-tls
+    k8Resources="webhook-deployment.yaml"
     for k8Resource in ${k8Resources}
     do
         sed -e "s@\$TARGET_NAMESPACE@$namespace@g" \
             -e "s@\$WEBHOOK_IMAGE@$image@g" \
             ${resourcesDir}/${k8Resource} > ${tmpDir}/${k8Resource}
-        kubectl --context ${context} -n ${namespace} apply -f ${tmpDir}/${k8Resource}
+        cat ${tmpDir}/${k8Resource}
     done
+
+    kubectl --context ${context} -n ${namespace} apply -f ${tmpDir}/
 
     waitForDeployment ${context} ${namespace} ${deployment}
 }
