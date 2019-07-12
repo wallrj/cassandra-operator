7b8accd5fb75d2c6f1901a2689a423e3b58536ab
Richard Wall <richard.wall@jetstack.io>
2019-07-12 17:49:44 +0100

    Merge BeforeSuite functions

--- cassandra-operator/test/e2e/sequential/validation/validation_test.go
+++ cassandra-operator/test/e2e/sequential/validation/validation_test.go
@@ -17,8 +17,6 @@ func TestValidation(t *testing.T) {
 	RunSpecsWithDefaultAndCustomReporters(t, "E2E Suite (Validation Tests)", test.CreateSequentialReporters("e2e_validation"))
 }
 
-var _ = e2e.SequentialTestBeforeSuite(func() {})
-
 // The defaulting webhook is called before the OpenAPI validation occurs,
 // See https://kubernetes.io/blog/2019/03/21/a-guide-to-kubernetes-admission-controllers/
 // So the invalid manifests that we attempt to apply here, will instead be rejected by the defaulting webhook,
@@ -26,8 +24,7 @@ var _ = e2e.SequentialTestBeforeSuite(func() {})
 // This is fixed in Kubernetes 1.15, which introduces a structural schema  validation step before the defaulting webhook is called.
 // See: https://kubernetes.io/blog/2019/06/20/crd-structural-schema/
 // To work around this, we temporarily reconfigure the deployed webhook, giving it a non-matching namespaceSelector.
-
-var _ = BeforeSuite(func() {
+var _ = e2e.SequentialTestBeforeSuite(func() {
 	command, output, err := e2e.Kubectl(
 		"",
 		"patch",
